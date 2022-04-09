185416aa6be9d82ece1f24f37ac79bff931acf83
Riquwu <ariqbimantoro@gmail.com>
2022-04-09 20:22:47 +0700

    Add search function

--- controllers/publicFeed.js
+++ controllers/publicFeed.js
@@ -1,7 +1,7 @@
 // Axios module to fetch Flickr API
 import axios from "axios";
 // API Constants
-import { URL, FORMAT_FEED } from "../constants/constants.js";
+import { URL, FORMAT_FEED, KEY, SEARCH } from "../constants/constants.js";
 
 // Flickr API
 export const getFeed = async (req, res) => {
@@ -16,6 +16,23 @@ export const getFeed = async (req, res) => {
         res.send({ err });
       });
   } catch (error) {
-    console.log(error);
+    res.status(500).json("Error");
+  }
+};
+
+export const searchFeed = async (req, res, { tags, per_page, page, text }) => {
+  try {
+    await axios
+      .get(
+        `${URL}/services/rest/?method=${SEARCH}&${KEY}&tags=${tags}&text=${text}&per_page=${per_page}&page=${page}&${FORMAT_FEED}&nojsoncallback=1`
+      )
+      .then((response) => {
+        res.send(response.data);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  } catch (error) {
+    res.status(500).json("Error");
   }
 };
