0b91eca7839497c24f48db61a13b417100d1ecc9
Riquwu <ariqbimantoro@gmail.com>
2022-04-09 18:59:41 +0700

    Add constants API

---
+++ constants/constants.js
@@ -0,0 +1,8 @@
+// My Config Constants
+
+// URL Flickr API
+export const URL = "https://api.flickr.com/";
+// Flickr API Key
+export const KEY = "api_key=ad07df90d9f71ef14810f7b245e60b70";
+// Flickr API Format Feed
+export const FORMAT_FEED = "format=json";
