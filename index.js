d3f02cc456922e3bb5540b9ee201ccbff6438251
Riquwu <ariqbimantoro@gmail.com>
2022-04-09 20:21:24 +0700

    Update index detail

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ import cors from "cors";
 import dotenv from "dotenv";
 // Import routes
 import feedRoutes from "./routes/publicFeed.js";
-
+// Config Setup
 const app = express();
-const PORT = 5000;
 dotenv.config();
-
+const PORT = process.env.PORT;
 // Routing Setup
 app.use("/feed", feedRoutes);
-
+// Cors Middleware for cross-origin
 app.use(cors());
-
+// Check server if it's running well
 app.get("/", (req, res) => {
   res.send("Ckrilf API");
 });
