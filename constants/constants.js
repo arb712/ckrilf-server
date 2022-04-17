51873ed879903f03f245636b6a7c8821dca5be7a
Riquwu <ariqbimantoro@gmail.com>
2022-04-17 15:47:12 +0700

    Update constants

--- constants/constants.js
+++ constants/constants.js
@@ -2,9 +2,5 @@
 
 // URL Flickr API
 export const URL = "https://api.flickr.com/";
-// Flickr API Key
-export const KEY = "api_key=ad07df90d9f71ef14810f7b245e60b70";
 // Flickr API Format Feed
 export const FORMAT_FEED = "format=json";
-// Search Method
-export const SEARCH = "flickr.photos.search";
